5d84145b4d4fdd542a5340f31d4eeeb05447438e
Ilikemebetter <pleum.41733@gmail.com>
2024-12-17 17:21:55 +0700

    lab10_3 completed

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,7 +1,25 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
 
+using namespace std;
 
-
-
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+int main(){
+    ifstream source;
+    string textline;
+    source.open("score.txt");
+    double sum = 0;
+    int numdata = 0;
+    double sd = 0;
+    while (getline(source, textline)){
+        sum += stod(textline);
+        sd += pow(stod(textline),2);
+        numdata++;
+    }
+        
+    cout << "Number of data = " << numdata <<endl;
+    cout << setprecision(3);
+    cout << "Mean = " << sum/numdata << endl;
+    cout << "Standard deviation = " << sqrt((sd/numdata)-(pow(sum/numdata,2)));
+}
